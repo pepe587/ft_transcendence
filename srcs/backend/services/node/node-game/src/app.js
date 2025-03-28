9e606f7aabef09f9ed8d2b52c80580e29067d618
jmartos- <jcmv.42malaga@gmail.com>
2025-03-28 17:44:52 +0100

    node-game y sqlite-login creados y funcionando

---
+++ srcs/backend/services/node/node-game/src/app.js
@@ -0,0 +1,12 @@
+// Esta es la entrada del microservicio
+
+// Desde aqui se crea el servidor, se cargan configuraciones, rutas, etc.
+
+async function main()
+{
+    console.log('Hola, soy el microservicio del juego');
+    
+    while (true) {}
+}
+  
+main();
